90f9c9d796b91e95292acf618c2559bdfda024ba
rodar024 <rodartee.m@gmail.com>
2025-04-23 13:03:58 -0700

    updated the food_list file by creating a json file. Allowing the user to edit the list by having access to its own document. Corrected the positioning of Qt design.

--- ui/My_Fitness_App.py
+++ ui/My_Fitness_App.py
@@ -1,9 +1,18 @@
 import sys
-# import math
+import json
 
-from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QCheckBox
+from PySide6.QtWidgets import (
+    QApplication, 
+    QMainWindow,
+    QCheckBox, 
+    QListWidgetItem,
+    QMessageBox
+)
 from main_ui import Ui_MainWindow
-from food_list import foods
+
+with open ("foods.json", "r") as file:
+    foods= json.load(file)
+#need to then create another file for foods. 
 
 daily_goal= 2000
 cal_buffer = 0
@@ -15,9 +24,10 @@ class My_APP (Ui_MainWindow, QMainWindow):
         self.current_cals = 0
 
         self.setupUi(self)
+        self.w = None
 
         self.generate_checkboxes()
-        self.pushButton_ok.clicked.connect(lambda: self.total_value())
+        self.pushButton_ok.clicked.connect(lambda: self.total_value_cal())
         self.progressBar.setRange(0, 2000)
         self.progressBar.setValue(0)
         self.pushButton_cancel.clicked.connect(lambda: self.canceled())
@@ -30,38 +40,58 @@ class My_APP (Ui_MainWindow, QMainWindow):
     def generate_checkboxes(self):
         for food in foods:
             checkbox = CustomCheckBox(food)
+            item = QListWidgetItem()
             type = foods[food]["type"]
-            if type == "Protein":
-                self.listView_fruit.addWidget(checkbox)
-            elif type =="Fruit":
-                self.listView_protein.addWidget(checkbox)
+            if type == "Fruit":
+                self.listWidget_fruit.addItem(item) #missing something
+                self.listWidget_fruit.setItemWidget(item, checkbox) 
+                #originally had: self.listWidget_fruit.addWidget(checkbox)
+            elif type =="Protein":
+                self.listWidget_protein.addItem(item)
+                self.listWidget_protein.setItemWidget(item, checkbox)
             elif type== "Vegetables":
-                self.listView_vegetables.addWidget(checkbox)
+                self.listWidget_vegetables.addItem(item)
+                self.listWidget_vegetables.setItemWidget(item, checkbox)
    
 
     #calculating the calories left and seeing if it over exceeds the goal
 
-    def total_value(self):
+    def total_value_cal(self):
         global cal_buffer
         global daily_goal
 
-        self.current_cals= 0
         total_cal= daily_goal - cal_buffer
         if total_cal < 0:
             print("You have exceeded your daily goal!")
+            ret = self.warning_box()
+            if not ret:
+                return
         else:
             print("You have these many calories left: " + str(total_cal) + "\nYour current Calories: " + str(cal_buffer))
+        
+        self.current_cals= 0
         self.current_cals +=cal_buffer
+        self.label_num_cal.setText(str(total_cal))
         self.update_progress_bar()
     
     def update_progress_bar(self): 
         self.progressBar.setValue(self.current_cals)
         if self.current_cals> daily_goal:
             self.progressBar.setValue(2000)
-
-    def canceled(self):
-        print ("You have canceled.")
-        self.progressBar.reset()
+    
+    def warning_box(self):
+        cal_warning= QMessageBox.warning(
+            self,
+            "Warning",
+            "You are about to exceed daily goal. Do you want to continue?",
+            buttons =QMessageBox.StandardButton.Yes,
+            defaultButton= QMessageBox.StandardButton.No,
+        )
+        if cal_warning == QMessageBox.StandardButton.Yes:
+            self.update_progress_bar()
+            return True
+        elif cal_warning == QMessageBox.StandardButton.No:
+            return False
 
 class CustomCheckBox(QCheckBox):
     def __init__(self, text, parent=None):
--- ui/main_ui.py
+++ ui/main_ui.py
@@ -16,93 +16,28 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QDateEdit, QGridLayout, QHBoxLayout,
-    QLabel, QListView, QMainWindow, QMenuBar,
-    QProgressBar, QPushButton, QSizePolicy, QSpacerItem,
-    QStatusBar, QWidget)
+    QLabel, QListWidget, QListWidgetItem, QMainWindow,
+    QMenuBar, QProgressBar, QPushButton, QSizePolicy,
+    QSpacerItem, QStatusBar, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(520, 364)
+        MainWindow.resize(566, 399)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.gridLayout = QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.label_title = QLabel(self.centralwidget)
-        self.label_title.setObjectName(u"label_title")
-        font = QFont()
-        font.setFamilies([u"Trebuchet MS"])
-        font.setPointSize(16)
-        font.setBold(True)
-        font.setUnderline(True)
-        self.label_title.setFont(font)
+        self.listWidget_fruit = QListWidget(self.centralwidget)
+        self.listWidget_fruit.setObjectName(u"listWidget_fruit")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.listWidget_fruit.sizePolicy().hasHeightForWidth())
+        self.listWidget_fruit.setSizePolicy(sizePolicy)
 
-        self.gridLayout.addWidget(self.label_title, 0, 2, 1, 1)
-
-        self.dateEdit = QDateEdit(self.centralwidget)
-        self.dateEdit.setObjectName(u"dateEdit")
-        self.dateEdit.setCalendarPopup(True)
-
-        self.gridLayout.addWidget(self.dateEdit, 0, 0, 1, 1)
-
-        self.listView_vegetables = QListView(self.centralwidget)
-        self.listView_vegetables.setObjectName(u"listView_vegetables")
-
-        self.gridLayout.addWidget(self.listView_vegetables, 3, 4, 1, 2)
-
-        self.label_daily_goal_title = QLabel(self.centralwidget)
-        self.label_daily_goal_title.setObjectName(u"label_daily_goal_title")
-        font1 = QFont()
-        font1.setFamilies([u"Trebuchet MS"])
-        font1.setPointSize(12)
-        font1.setBold(True)
-        self.label_daily_goal_title.setFont(font1)
-
-        self.gridLayout.addWidget(self.label_daily_goal_title, 0, 4, 1, 1)
-
-        self.label_fruit = QLabel(self.centralwidget)
-        self.label_fruit.setObjectName(u"label_fruit")
-        font2 = QFont()
-        font2.setFamilies([u"Trebuchet MS"])
-        font2.setPointSize(10)
-        font2.setBold(True)
-        font2.setItalic(True)
-        self.label_fruit.setFont(font2)
-
-        self.gridLayout.addWidget(self.label_fruit, 2, 2, 1, 1)
-
-        self.listView_protein = QListView(self.centralwidget)
-        self.listView_protein.setObjectName(u"listView_protein")
-
-        self.gridLayout.addWidget(self.listView_protein, 3, 0, 1, 2)
-
-        self.label_Protein = QLabel(self.centralwidget)
-        self.label_Protein.setObjectName(u"label_Protein")
-        self.label_Protein.setFont(font2)
-
-        self.gridLayout.addWidget(self.label_Protein, 2, 0, 1, 1)
-
-        self.label_num_cal = QLabel(self.centralwidget)
-        self.label_num_cal.setObjectName(u"label_num_cal")
-        font3 = QFont()
-        font3.setFamilies([u"Trebuchet MS"])
-        font3.setPointSize(11)
-        self.label_num_cal.setFont(font3)
-
-        self.gridLayout.addWidget(self.label_num_cal, 0, 5, 1, 1)
-
-        self.progressBar = QProgressBar(self.centralwidget)
-        self.progressBar.setObjectName(u"progressBar")
-        self.progressBar.setValue(24)
-
-        self.gridLayout.addWidget(self.progressBar, 4, 0, 1, 2)
-
-        self.label_Vegetables = QLabel(self.centralwidget)
-        self.label_Vegetables.setObjectName(u"label_Vegetables")
-        self.label_Vegetables.setFont(font2)
-
-        self.gridLayout.addWidget(self.label_Vegetables, 2, 4, 1, 1)
+        self.gridLayout.addWidget(self.listWidget_fruit, 3, 2, 1, 2)
 
         self.horizontalLayout_3 = QHBoxLayout()
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
@@ -121,33 +56,102 @@ class Ui_MainWindow(object):
         self.horizontalLayout_3.addWidget(self.pushButton_cancel)
 
 
-        self.gridLayout.addLayout(self.horizontalLayout_3, 4, 2, 1, 2)
+        self.gridLayout.addLayout(self.horizontalLayout_3, 6, 4, 1, 1)
 
-        self.listView_fruit = QListView(self.centralwidget)
-        self.listView_fruit.setObjectName(u"listView_fruit")
-        sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.listView_fruit.sizePolicy().hasHeightForWidth())
-        self.listView_fruit.setSizePolicy(sizePolicy)
+        self.verticalSpacer = QSpacerItem(20, 20, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Maximum)
 
-        self.gridLayout.addWidget(self.listView_fruit, 3, 2, 1, 2)
+        self.gridLayout.addItem(self.verticalSpacer, 1, 1, 1, 1)
+
+        self.label_Vegetables = QLabel(self.centralwidget)
+        self.label_Vegetables.setObjectName(u"label_Vegetables")
+        font = QFont()
+        font.setFamilies([u"Trebuchet MS"])
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setItalic(True)
+        self.label_Vegetables.setFont(font)
+
+        self.gridLayout.addWidget(self.label_Vegetables, 2, 4, 1, 1)
+
+        self.dateEdit = QDateEdit(self.centralwidget)
+        self.dateEdit.setObjectName(u"dateEdit")
+        self.dateEdit.setCalendarPopup(True)
+
+        self.gridLayout.addWidget(self.dateEdit, 0, 0, 1, 1)
+
+        self.listWidget_protein = QListWidget(self.centralwidget)
+        self.listWidget_protein.setObjectName(u"listWidget_protein")
+
+        self.gridLayout.addWidget(self.listWidget_protein, 3, 0, 1, 2)
+
+        self.label_Protein = QLabel(self.centralwidget)
+        self.label_Protein.setObjectName(u"label_Protein")
+        self.label_Protein.setFont(font)
+
+        self.gridLayout.addWidget(self.label_Protein, 2, 0, 1, 1)
+
+        self.label_fruit = QLabel(self.centralwidget)
+        self.label_fruit.setObjectName(u"label_fruit")
+        self.label_fruit.setFont(font)
+
+        self.gridLayout.addWidget(self.label_fruit, 2, 2, 1, 1)
+
+        self.label_num_cal = QLabel(self.centralwidget)
+        self.label_num_cal.setObjectName(u"label_num_cal")
+        font1 = QFont()
+        font1.setFamilies([u"Trebuchet MS"])
+        font1.setPointSize(11)
+        self.label_num_cal.setFont(font1)
+
+        self.gridLayout.addWidget(self.label_num_cal, 5, 0, 1, 1)
+
+        self.progressBar = QProgressBar(self.centralwidget)
+        self.progressBar.setObjectName(u"progressBar")
+        self.progressBar.setValue(24)
+
+        self.gridLayout.addWidget(self.progressBar, 6, 0, 1, 2)
+
+        self.listWidget_vegetables = QListWidget(self.centralwidget)
+        self.listWidget_vegetables.setObjectName(u"listWidget_vegetables")
+
+        self.gridLayout.addWidget(self.listWidget_vegetables, 3, 4, 1, 2)
+
+        self.label_daily_goal_title = QLabel(self.centralwidget)
+        self.label_daily_goal_title.setObjectName(u"label_daily_goal_title")
+        font2 = QFont()
+        font2.setFamilies([u"Trebuchet MS"])
+        font2.setPointSize(12)
+        font2.setBold(True)
+        self.label_daily_goal_title.setFont(font2)
+
+        self.gridLayout.addWidget(self.label_daily_goal_title, 4, 0, 1, 1)
+
+        self.label_app_title = QLabel(self.centralwidget)
+        self.label_app_title.setObjectName(u"label_app_title")
+        font3 = QFont()
+        font3.setFamilies([u"Trebuchet MS"])
+        font3.setPointSize(16)
+        font3.setBold(True)
+        font3.setUnderline(True)
+        self.label_app_title.setFont(font3)
+
+        self.gridLayout.addWidget(self.label_app_title, 0, 2, 1, 1)
 
         MainWindow.setCentralWidget(self.centralwidget)
-        self.listView_protein.raise_()
-        self.listView_fruit.raise_()
-        self.listView_vegetables.raise_()
+        self.listWidget_protein.raise_()
+        self.listWidget_fruit.raise_()
+        self.listWidget_vegetables.raise_()
         self.progressBar.raise_()
         self.label_fruit.raise_()
         self.label_Vegetables.raise_()
         self.dateEdit.raise_()
-        self.label_daily_goal_title.raise_()
-        self.label_num_cal.raise_()
-        self.label_title.raise_()
         self.label_Protein.raise_()
+        self.label_num_cal.raise_()
+        self.label_daily_goal_title.raise_()
+        self.label_app_title.raise_()
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 520, 22))
+        self.menubar.setGeometry(QRect(0, 0, 566, 22))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -160,13 +164,13 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.label_title.setText(QCoreApplication.translate("MainWindow", u"My Fitness", None))
-        self.label_daily_goal_title.setText(QCoreApplication.translate("MainWindow", u"Daily Goal:", None))
-        self.label_fruit.setText(QCoreApplication.translate("MainWindow", u"Fruit:", None))
-        self.label_Protein.setText(QCoreApplication.translate("MainWindow", u"Protein:", None))
-        self.label_num_cal.setText(QCoreApplication.translate("MainWindow", u"2,000", None))
-        self.label_Vegetables.setText(QCoreApplication.translate("MainWindow", u"Vegetables:", None))
         self.pushButton_ok.setText(QCoreApplication.translate("MainWindow", u"OK", None))
         self.pushButton_cancel.setText(QCoreApplication.translate("MainWindow", u"Cancel", None))
+        self.label_Vegetables.setText(QCoreApplication.translate("MainWindow", u"Vegetables:", None))
+        self.label_Protein.setText(QCoreApplication.translate("MainWindow", u"Protein:", None))
+        self.label_fruit.setText(QCoreApplication.translate("MainWindow", u"Fruit:", None))
+        self.label_num_cal.setText(QCoreApplication.translate("MainWindow", u"2,000", None))
+        self.label_daily_goal_title.setText(QCoreApplication.translate("MainWindow", u"Daily Goal:", None))
+        self.label_app_title.setText(QCoreApplication.translate("MainWindow", u"My Fitness", None))
     # retranslateUi
 
